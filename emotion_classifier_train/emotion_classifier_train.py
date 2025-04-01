b6dffa3fddd701694a6bb8e616444c00787e37dd
ATLASO4EK <126068818+ATLASO4EK@users.noreply.github.com>
2025-04-01 19:37:17 +0700

    Add files via upload

--- emotion_classifier_train/emotion_classifier_train.py
+++ emotion_classifier_train/emotion_classifier_train.py
@@ -1,26 +1,44 @@
 import pandas as pd
 import numpy as np
 import torch.nn as nn
-
-
+import torch.utils.data as data
 
 # Загружаем датасет
-# Я 1.5 ПАРЫ УДАЛЯЛ НЕНУЖНЫЕ СТОЛБЦЫ ИЗ ДАТАСЕТА Я ТОГО ВСË
 train_df = pd.read_csv('train.csv')
+
+# Создаем класс датасета
+class EmotionDataset(data.Dataset):
+    def __init__(self):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self, item):
+        pass
 
 # Создаем модель
 class Emotion_Classifier(nn.Module):
     def __init__(self):
-        self.LSTM = nn.LSTM(input_size=1000, hidden_size=32)
-        self.layer1 = nn.Linear(in_features=32, out_features=6)
+        super().__init__()
+        self.embed = nn.Embedding()
+        self.rls = nn.Sequential(nn.RNN(),
+                                nn.Tanh())
+        self.dls = nn.Sequential(nn.Linear(in_features=32, out_features=6),
+                                 nn.ReLU())
+        self.softmax = nn.Softmax()
 
     def forward(self, x):
-        x = self.LSTM(x)
-        x = self.layer1(x)
+        x = self.embed(x)
+        x = self.rls(x)
+        x = self.dls(x)
+        x = self.softmax(x)
 
         return x
 
 # Обучаем + сохраняем результат (модель + веса)
+d_train = EmotionDataset()
+train_data = data.DataLoader(d_train, batch_size=4, shuffle=True)
+
+
 model = Emotion_Classifier()
 epochs = 100
 model.train()
