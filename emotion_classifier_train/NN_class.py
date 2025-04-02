09aaf6056a42484b30a637bac22f2179795febdb
ATLASO4EK <126068818+ATLASO4EK@users.noreply.github.com>
2025-04-02 19:15:05 +0700

    Add files via upload

---
+++ emotion_classifier_train/NN_class.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+from transformers import BertModel
+
+class EmotionClassifier(nn.Module):
+    def __init__(self, n_classes):
+        super(EmotionClassifier, n_classes).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.drop = nn.Dropout(p=0.3)
+        self.linear = nn.Linear(self.bert.config.hidden_size, n_classes)
+        self.softmax = nn.Softmax()
+
+
+    def forward(self, input_ids, attention_mask):
+        _, pooled_output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        output = self.drop(pooled_output)
+        output = self.linear(output)
+        output = self.softmax(output)
+
+        return output
--- emotion_classifier_train/emotion_classifier_train.py
+++ emotion_classifier_train/emotion_classifier_train.py
@@ -1,47 +1,96 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
+import torch
 import torch.nn as nn
 import torch.utils.data as data
+from torch.optim import AdamW
+from transformers import BertModel, BertTokenizer, get_linear_schedule_with_warmup
+
+from dataset import EmotionDataset
+from NN_class import EmotionClassifier
 
 # Загружаем датасет
-train_df = pd.read_csv('train.csv')
+train_df = pd.read_csv('train_splitted.csv')
+val_df = pd.read_csv('val_splitted.csv')
 
-# Создаем класс датасета
-class EmotionDataset(data.Dataset):
-    def __init__(self):
-        pass
-    def __len__(self):
-        pass
-    def __getitem__(self, item):
-        pass
+# "DATA PREPROCESSING" - после этих слов аниме продлилось на ещё 1000 серий
+PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
+tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+max_len = 256
 
-# Создаем модель
-class Emotion_Classifier(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.embed = nn.Embedding()
-        self.rls = nn.Sequential(nn.RNN(),
-                                nn.Tanh())
-        self.dls = nn.Sequential(nn.Linear(in_features=32, out_features=6),
-                                 nn.ReLU())
-        self.softmax = nn.Softmax()
-
-    def forward(self, x):
-        x = self.embed(x)
-        x = self.rls(x)
-        x = self.dls(x)
-        x = self.softmax(x)
-
-        return x
-
-# Обучаем + сохраняем результат (модель + веса)
+### Обучаем + сохраняем результат (модель + веса)
 d_train = EmotionDataset()
-train_data = data.DataLoader(d_train, batch_size=4, shuffle=True)
+train_dataloader = data.DataLoader(d_train, batch_size=4, shuffle=True, num_workers=4)
 
+val_data = EmotionDataset()
+val_dataloader = data.DataLoader(val_data, batch_size=1, shuffle=False, num_workers=4)
 
-model = Emotion_Classifier()
+# Выбираем устройство
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# Создаем объект модели, оптимизатора и тд.
 epochs = 100
-model.train()
 
+model = EmotionClassifier(n_classes=6).to(device)
+optimizer = AdamW(model.parameters(), lr=2e-5, correct_bias=False)
+total_steps = len(train_dataloader) * epochs
+scheduler = get_linear_schedule_with_warmup(optimizer,
+                                            num_warmup_steps=0,
+                                            num_training_steps=total_steps)
+loss_func = nn.CrossEntropyLoss().to(device)
+
+def train_epoch(model, optimizer, scheduler, x, y, lm_count, loss_mean):
+    model = model.train()
+    pred = torch.argmax(model(x).to(device))
+    loss = loss_func(pred, y)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    scheduler.step()
+    lm_count += 1
+    loss_mean = 1 / lm_count * loss.item() + (1 - 1 / lm_count) * loss_mean
+
+    return loss_mean
+
+def valid_model(model, dataloader, loss_func):
+    model = model.eval()
+    losses = []
+    correct_predictions = 0
+
+    with torch.no_grad():
+        for x, y in dataloader:
+
+            pred = model(x).to(device)
+            pred_item = torch.argmax(pred).item()
+            label = torch.argmax(y).item()
+            loss = loss_func(pred, y)
+
+            correct_predictions += torch.sum(pred_item == label)
+            losses.append(loss.item())
+
+    val_acc = correct_predictions.double() / len(dataloader)
+    val_loss = np.mean(losses)
+
+    return val_acc, val_loss
+
+#Запуск процесса обучения
 for epoch in range(epochs):
-    pass
+    train_tqdm = tqdm(train_dataloader, leave=True)
+    lm_count = 0
+    loss_mean = 0
+
+    for x, y in train_tqdm:
+        loss_mean = train_epoch(model, optimizer, scheduler, x, y, lm_count, loss_mean)
+        train_tqdm.set_description(f"Epoch [{epoch + 1}/{epochs}], loss_mean={loss_mean:.3f}")
+
+    # Валидация + сохранение норм моделей
+    best_acc = 0
+    val_acc, val_loss = valid_model(model, val_dataloader, loss_func)
+
+    if val_acc>best_acc:
+        save_data = [model, model.state_dict()]
+        torch.save(save_data, f=f'models/emotion_classifier_epoch{epoch+1}')
+
+
